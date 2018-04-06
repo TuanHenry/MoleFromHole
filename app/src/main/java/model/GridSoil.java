e6c5dca5599cec3f65d0ec936375e43258a7d392
TuanHenry <boypin392@gmail.com>
2018-04-06 23:32:58 +0700

    The first times

---
+++ app/src/main/java/model/GridSoil.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Henry on 4/6/2018.
+ */
+
+public class GridSoil {
+    private ArrayList<Integer> arraySoil;//0: đất trống 1: đất có hố quét mảng truyền vào để tạo lưới ô đất
+    private int mWidthOfLand;   //columns được truyền vào
+    private int mHeightOfLand;  //rows được truyền vào
+
+
+    //tạo ra lưới chứa các hole theo số lượng được truyền vào
+    // add lưới vào grid_map(GridView trong file mainactivity.xml
+    //test riêng từng phần kiểm tra hiển thị đúng yêu cầu
+    //có thể để giá trị imgHole là hình bất kì với kích thước bất kì để test
+
+    //hình ảnh
+//    https://drive.google.com/drive/folders/16MRcTjyD1BF4NKF-d6nVMcCjY14zmJhQ?usp=sharing
+
+}
