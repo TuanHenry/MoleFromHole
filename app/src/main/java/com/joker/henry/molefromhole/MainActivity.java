12eab0661426dc4f4f9da1241fb4e0231c6d6a42
Your Name <you@example.com>
2018-04-07 22:38:40 +0700

    commit changed

--- app/src/main/java/com/joker/henry/molefromhole/MainActivity.java
+++ app/src/main/java/com/joker/henry/molefromhole/MainActivity.java
@@ -1,13 +1,57 @@
 package com.joker.henry.molefromhole;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+import model.HinhanhAdapter;
+import model.GridSoil;
+import model.HinhAnh;
 
 public class MainActivity extends AppCompatActivity {
 
+    GridView grvBan;
+    GridSoil gridSoil=new GridSoil(10, 8);
+
+    ArrayList<HinhAnh> listHinhAnh;
+
+    HinhanhAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listHinhAnh =new ArrayList<>();
+
+
+        grvBan=(GridView) findViewById(R.id.grid);
+        VeManHinh();
+    }
+
+    public void VeManHinh()
+    {
+
+
+        grvBan.setNumColumns(gridSoil.getmWidthOfLand());
+
+        gridSoil.Init(9);
+
+
+
+        for(int i=0; i< gridSoil.getmHeightOfLand()*gridSoil.getmWidthOfLand(); i++){
+
+            if(gridSoil.getArraySoil(i)==1)
+            {
+                listHinhAnh.add(new HinhAnh(R.drawable.chuotlone));
+            }
+            else
+                listHinhAnh.add(new HinhAnh(R.drawable.lo));
+
+        }
+
+
+        adapter=new HinhanhAdapter(this, R.layout.hinh_anh,listHinhAnh );
+        grvBan.setAdapter(adapter);
     }
 }
--- app/src/main/java/model/GridSoil.java
+++ app/src/main/java/model/GridSoil.java
@@ -1,20 +1,86 @@
 package model;
 
-import java.util.ArrayList;
+
+import java.util.Random;
 
 /**
  * Created by Henry on 4/6/2018.
  */
 
 public class GridSoil {
-    private ArrayList<Integer> arraySoil;//0: đất trống 1: đất có hố quét mảng truyền vào để tạo lưới ô đất
+    private int arraySoil[] = new int[150];// trống 1: đất có hố quét mảng truyền vào để tạo lưới ô đất
+
+    public int getArraySoil(int x) {
+        return arraySoil[x];
+    }
+
+    public void setArraySoil(int index, int number) {
+        this.arraySoil[index] = number;
+    }
+
+    public GridSoil(int mWidthOfLand, int mHeightOfLand) {
+        this.mWidthOfLand = mWidthOfLand;
+        this.mHeightOfLand = mHeightOfLand;
+    }
+
+
+
     private int mWidthOfLand;   //columns được truyền vào
+
+
+
+
+    public int getmWidthOfLand() {
+        return mWidthOfLand;
+    }
+
+    public void setmWidthOfLand(int mWidthOfLand) {
+        this.mWidthOfLand = mWidthOfLand;
+    }
+
+    public int getmHeightOfLand() {
+        return mHeightOfLand;
+    }
+
+    public void setmHeightOfLand(int mHeightOfLand) {
+        this.mHeightOfLand = mHeightOfLand;
+    }
+
     private int mHeightOfLand;  //rows được truyền vào
 
 
     //tạo ra lưới chứa các hole theo số lượng được truyền vào
+    public  void Init(int holes)
+    {
+        int s=0;
+        for(int i=1; i< getmHeightOfLand()*getmWidthOfLand(); i++){
+
+
+            this.arraySoil[i] = 0;
+
+        }
+        Random rd = new Random();
+        int dem=0;
+        int x;
+        while(dem<holes)
+        {
+            x = rd.nextInt(getmHeightOfLand()*getmWidthOfLand());
+
+            if (arraySoil[x] == 0)
+            {
+                arraySoil[x]=1;
+                dem++;
+
+            }
+        }
+
+    }
+
+
     // add lưới vào grid_map(GridView trong file mainactivity.xml
+
     //test riêng từng phần kiểm tra hiển thị đúng yêu cầu
+
     //có thể để giá trị imgHole là hình bất kì với kích thước bất kì để test
 
     //hình ảnh
